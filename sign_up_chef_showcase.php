d269b2889a11ef03d3e044d1ce07fd96d3b92ea5
abridges2 <aidan.bridges@shaw.ca>
2023-12-12 05:15:13 -0600

    added php code to handle the logic for inserting chef information into the chefs table.

--- sign_up_chef_showcase.php
+++ sign_up_chef_showcase.php
@@ -1,3 +1,32 @@
+<?php
+session_start();
+
+// Check if the user is of type chef.
+if(isset($_POST['submit']))
+{
+    // Input Validation
+    $chef_name = filter_input(INPUT_POST, 'chef_name', FILTER_SANITIZE_STRING);
+    $chef_age = filter_input(INPUT_POST, 'chef_age', FILTER_VALIDATE_NUMBER_INT);
+    $restaurant_name = filter_input(INPUT_POST, 'restaurant_name', FILTER_SANITIZE_STRING);
+    $restaurant_location = filter_input(INPUT_POST, 'restaurant_location', FILTER_SANITIZE_STRING);
+    $biography = filter_input(INPUT_POST, 'biography', FILTER_SANITIZE_STRING);
+
+
+    $query = "INSERT INTO chefs(chef_name, chef_age, restaraunt_name, restaraunt_location, biography)
+        VALUES (:chef_name, :chef_age, :restaraunt_name, :restaraunt_location, :biography)";
+    $statement = $db->prepare($query);
+
+    // Bind Parameters
+    $statement->bindValue(':chef_name', $chef_name);
+    $statement->bindValue(':chef_age', $chef_age);
+    $statement->bindValue(':restaurant_name', $restaurant_name);
+    $statement->bindValue(':restaurant_location', $restaurant_location); 
+    $statement->bindValue(':biography', $biography);
+
+    header("Location: chef_showcase.php");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,22 +36,28 @@
 </head>
 <body>
     <form action="sign_up_chef_showcase.php" method="post">
-        <!-- Chef information -->
-        <label for="chef_name">Name:</label>
-        <input type="text" name="chef_name" id="chef_name">
+        <?php if($_SESSION['user_type'] == 'chef'): ?>
+            <!-- Chef information -->
+            <label for="chef_name">Name:</label>
+            <input type="text" name="chef_name" id="chef_name">
 
-        <label for="chef_age">Age:</label>
-        <input type="text" name="chef_age" id="chef_age">
+            <label for="chef_age">Age:</label>
+            <input type="text" name="chef_age" id="chef_age">
 
-        <label for="restaraunt_name">Restaurant Name:</label>
-        <input type="text" name="restaraunt_name" id="restaraunt_name">
+            <label for="restaurant_name">Restaurant Name:</label>
+            <input type="text" name="restaurant_name" id="restaurant_name">
 
-        <label for="restaraunt_location">Restaurant Location:</label>
-        <input type="text" name="restaraunt_location" id="restaraunt_location">
+            <label for="restaurant_location">Restaurant Location:</label>
+            <input type="text" name="restaurant_location" id="restaurant_location">
 
-        <label for="restaraunt_name">Biography:</label>
-        <input type="text" name="biography" id="biography" placeholder="Tell us about yourself.">
+            <label for="restaraunt_name">Biography:</label>
+            <input type="text" name="biography" id="biography" placeholder="Tell us about yourself.">
 
+            <button type="submit">Submit</button>
+        
+        <?php else: ?>
+            <?= "Your user account is not registered as a chef." ?>
+        <?php endif; ?>        
     </form>
 </body>
 </html>
