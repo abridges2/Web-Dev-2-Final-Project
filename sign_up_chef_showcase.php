c81c699494057562f86d02f8d4d3be4c63385fc7
abridges2 <aidan.bridges@shaw.ca>
2023-12-12 02:39:06 -0600

    Added a new file for signing up for the chef showcase page on the site. HTML form created for signing up. Adding php to handle logic for the form soon.

---
+++ sign_up_chef_showcase.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="sign_up_chef_showcase.php" method="post">
+        <!-- Chef information -->
+        <label for="chef_name">Name:</label>
+        <input type="text" name="chef_name" id="chef_name">
+
+        <label for="chef_age">Age:</label>
+        <input type="text" name="chef_age" id="chef_age">
+
+        <label for="restaraunt_name">Restaurant Name:</label>
+        <input type="text" name="restaraunt_name" id="restaraunt_name">
+
+        <label for="restaraunt_location">Restaurant Location:</label>
+        <input type="text" name="restaraunt_location" id="restaraunt_location">
+
+        <label for="restaraunt_name">Biography:</label>
+        <input type="text" name="biography" id="biography" placeholder="Tell us about yourself.">
+
+    </form>
+</body>
+</html>
