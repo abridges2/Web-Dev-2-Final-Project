63454d92b48bf6d73c60c59c69a3a820f6865cdb
abridges2 <aidan.bridges@shaw.ca>
2023-12-12 01:52:12 -0600

    Added HTML to the chef_showcase page

--- chef_showcase.php
+++ chef_showcase.php
@@ -6,6 +6,47 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <header>
+        <h1>Welcome to the Chef Showcase!</h1>
+    </header>
+    <div>
+        <!-- Chef 1 -->
+        <div>
+            <img src="chef1.jpg" alt="Chef 1 Image">
+            <div>
+                <h5>Chef 1: Amber Meadows</h5>
+                <p>Amber Meadows makes delicous pizza in Winnipeg. Her specialty is
+                    the Royal Hawaiian pizza and it is publicly recognized as one of
+                    Winnipeg's favorites!
+                </p>
+            </div>
+        </div>
+
+        <!-- Chef 2 -->
+        <div>
+            <img src="chef2.jpg" alt="Chef 2 Image">
+            <div>
+                <h5>Chef 2: Matt Ewasiuk</h5>
+                <p>Sue Chef 1 at Earls St.Vital Matt Ewasiuk runs the show at night time
+                    in Earls St.Vital a fine establishment. He earns a spot on the chef showcase
+                    by making the best Dynamite Rolls in town!
+                </p>
+            </div>
+        </div>
+
+        <!-- Chef 3 -->
+        <div>
+            <img src="chef3.jpg" alt="Chef 3 Image">
+            <div>
+                <h5>Chef 3: Aidan Bridges</h5>
+                <p>Aidan Bridges makes the best bowls of cereal and milk in not just Winnipeg
+                    but the entire world! This earns him a spot on the chef showcase.
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
 </body>
 </html>
