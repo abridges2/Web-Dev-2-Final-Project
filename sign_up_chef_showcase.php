71b4145c1f3f738176480c52aec270cff217230b
abridges2 <aidan.bridges@shaw.ca>
2023-12-15 04:48:13 -0600

    Resolved error for form not submitting correctly by adding if statement checking if the form is being submitted correctly and if so, redirect.

--- sign_up_chef_showcase.php
+++ sign_up_chef_showcase.php
@@ -1,16 +1,16 @@
 <?php
 session_start();
+require('connect.php');
 
 // Check if the user is of type chef.
 if(isset($_POST['submit']))
 {
     // Input Validation
     $chef_name = filter_input(INPUT_POST, 'chef_name', FILTER_SANITIZE_STRING);
-    $chef_age = filter_input(INPUT_POST, 'chef_age', FILTER_VALIDATE_NUMBER_INT);
-    $restaurant_name = filter_input(INPUT_POST, 'restaurant_name', FILTER_SANITIZE_STRING);
-    $restaurant_location = filter_input(INPUT_POST, 'restaurant_location', FILTER_SANITIZE_STRING);
+    $chef_age = filter_input(INPUT_POST, 'chef_age', FILTER_VALIDATE_INT);
+    $restaraunt_name = filter_input(INPUT_POST, 'restaraunt_name', FILTER_SANITIZE_STRING);
+    $restaraunt_location = filter_input(INPUT_POST, 'restaraunt_location', FILTER_SANITIZE_STRING);
     $biography = filter_input(INPUT_POST, 'biography', FILTER_SANITIZE_STRING);
-
 
     $query = "INSERT INTO chefs(chef_name, chef_age, restaraunt_name, restaraunt_location, biography)
         VALUES (:chef_name, :chef_age, :restaraunt_name, :restaraunt_location, :biography)";
@@ -19,11 +19,21 @@ if(isset($_POST['submit']))
     // Bind Parameters
     $statement->bindValue(':chef_name', $chef_name);
     $statement->bindValue(':chef_age', $chef_age);
-    $statement->bindValue(':restaurant_name', $restaurant_name);
-    $statement->bindValue(':restaurant_location', $restaurant_location); 
+    $statement->bindValue(':restaraunt_name', $restaraunt_name);
+    $statement->bindValue(':restaraunt_location', $restaraunt_location); 
     $statement->bindValue(':biography', $biography);
 
-    header("Location: chef_showcase.php");
+    // Execute the query
+    if ($statement->execute()) 
+    {
+        // Redirect after successful submission
+        header("Location: chef_showcase.php");
+        exit();
+    } 
+    else 
+    {
+        echo "Error: Unable to submit chef information.";
+    }
 }
 ?>
 
@@ -44,16 +54,16 @@ if(isset($_POST['submit']))
             <label for="chef_age">Age:</label>
             <input type="text" name="chef_age" id="chef_age">
 
-            <label for="restaurant_name">Restaurant Name:</label>
-            <input type="text" name="restaurant_name" id="restaurant_name">
+            <label for="restaraunt_name">Restaraunt Name:</label>
+            <input type="text" name="restaraunt_name" id="restaraunt_name">
 
-            <label for="restaurant_location">Restaurant Location:</label>
-            <input type="text" name="restaurant_location" id="restaurant_location">
+            <label for="restaraunt_location">Restaraunt Location:</label>
+            <input type="text" name="restaraunt_location" id="restaraunt_location">
 
-            <label for="restaraunt_name">Biography:</label>
+            <label for="biography">Biography:</label>
             <input type="text" name="biography" id="biography" placeholder="Tell us about yourself.">
 
-            <button type="submit">Submit</button>
+            <button type="submit" name="submit">Submit</button>
         
         <?php else: ?>
             <?= "Your user account is not registered as a chef." ?>
